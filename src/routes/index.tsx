5a3ea62046f9064d45c36392d110f016e854b301
caarlosdamian <dapecarlos@gmail.com>
2023-02-21 00:09:03 -0600

    next deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  createHashRouter,
   Route,
 } from "react-router-dom";
 import { Header } from "../components/header/Header";
 import { Home } from "../pages/home/Home";
 import { SignUp } from "../pages/signup/SignUp";
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<Header />}>
       <Route index element={<Home />} />
